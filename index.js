c315911a498a66f2212a7adf3d91417f10e88868
Chris Oloff <christian.oloff@gmail.com>
2016-04-09 18:07:59 +0200

    cleanup, comments

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 var MongoDB = require('mongodb');
-var async = require('async');
 var parallel = require('concurrent-transform');
 var Transform = require('stream').Transform;
-var Writable = require('stream').Writable;
-var util = require('util');
 
 var numInflight = 0, numProcessed = 0;
 function doPotentiallyAsyncWork(doc, callback) {
-  console.log('numInflight', ++numInflight, 'numProcessed', numProcessed);
+  ++numInflight;
   setTimeout(function() {
-    --numInflight;
-    ++numProcessed;
-    callback(null);
+    console.log('numInflight', numInflight--, 'numProcessed', ++numProcessed);
+    callback(null); // as usual, if an error occurred, call the callback with anything truthy
   }, 200);
 }
 
 MongoDB.MongoClient.connect(process.env.MONGO_URL || 'mongodb://localhost/parallel-test', function(err, db) {
+
   if (err) throw err;
 
-  var cursor = db.collection('threads').find({});
+  var cursor = db.collection('threads').find({}); // use whatever query expression is needed (instead of `{}`)
+
+  // we need the stream interface of MongoDB
+  // (http://mongodb.github.io/node-mongodb-native/2.1/api/Cursor.html#stream)
   var stream = cursor.stream();
-  var nProcessing = 0;
 
   var transform = new Transform({
-    transform: function(chunk, encoding, next) {
-      console.log('transform', chunk);
-      doPotentiallyAsyncWork(chunk, function(err) {
-        console.log('processed data (simulated with a delay)', chunk);
-        next();
+    // `transform` will be called for each document. Here, any async processing can happen,
+    // but it's essential to call `next()` once everything is done.
+    transform: function(data, encoding, next) {
+      doPotentiallyAsyncWork(data, function(err) {
+        console.log('processed data (simulated with a delay)', data);
+        next(err);
       });
     },
     flush: function(done) {
@@ -38,15 +38,15 @@ MongoDB.MongoClient.connect(process.env.MONGO_URL || 'mongodb://localhost/parall
     objectMode: true
   });
 
-  stream.pipe(parallel(transform, 1000));
-
-  stream.on('data', function(data) {
-    // console.log('got data', data);
-  });
+  stream.pipe(parallel(transform, 100));
 
   stream.on('end', function() {
     console.log('stream end');
     db.close(); // do *not* close the database connection here, if the database `db` is re-used somewhere
+  });
+
+  transform.on('error', function() {
+    console.log('an error occurred');
   })
 
 });
